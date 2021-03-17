263a2b1a08abd81db53f6a49928247744d16d90a
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-03-18 00:25:57 +0100

    importing the local mock data and add a helper function to convert image names to kebab case

--- src/app/home-tab/tab1.page.ts
+++ src/app/home-tab/tab1.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 
+import recentlyPlayed from '../../assets/mockdata/recentlyPlayed.json';
+import heavyRotation from '../../assets/mockdata/heavyRotation.json';
+import jumpBackIn from '../../assets/mockdata/jumpBackIn.json';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  data = [
+    {
+      title: 'Recently played',
+      albums: recentlyPlayed,
+    },
+    {
+      title: 'Heavy rotation',
+      albums: heavyRotation,
+    },
+    {
+      title: 'Jump back in',
+      albums: jumpBackIn,
+    },
+  ];
 
   constructor() {}
 
+  // Helper function for image names
+  dasherize(string) {
+    return string.replace(/[A-Z]/g, function (char, index) {
+      return (index !== 0 ? '-' : '') + char.toLowerCase();
+    });
+  }
 }
