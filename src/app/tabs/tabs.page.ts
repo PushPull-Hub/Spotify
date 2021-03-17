748b84a94aef49c0240e4a263914ff8865696e04
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-03-17 23:42:51 +0100

    rendering the tab bar icon style up to it status

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { IonTabs } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  @ViewChild('tabs') tabs: IonTabs;
+  selectedTab: string = '';
 
   constructor() {}
 
+  setSelectedTab() {
+    this.selectedTab = this.tabs.getSelected();
+  }
 }
