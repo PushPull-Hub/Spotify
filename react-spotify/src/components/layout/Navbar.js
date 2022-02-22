37011a86b745e610193cda376680eac1a8eb95e8
D.Hamza <dahhani.hamza1998@gmail.com>
2022-02-22 14:47:49 +0100

    remove tabs and use route (for practice matter)

--- react-spotify/src/components/layout/Navbar.js
+++ react-spotify/src/components/layout/Navbar.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className='navbar navbar-dark bg-success mb-5'>
-      <span className='navbar-brand mb-0 h1 mx-auto'>Songify</span>
-    </nav>
-  )
+    <>
+      <nav className='navbar navbar-dark bg-success mb-5'>
+        <span className='navbar-brand mb-0 h1 mx-auto'>Songify</span>
+      </nav>
+      <div>
+        <ul className='nav nav-tabs'>
+          <li className='nav-item'>
+            <Link className='nav-link active' to='/'>
+              Songs
+            </Link>
+          </li>
+          <li className='nav-item'>
+            <Link className='nav-link' to='/favs'>
+              Favorites
+            </Link>
+          </li>
+          <li classNameclass='nav-item'>
+            <Link className='nav-link' to='/generator'>
+              Generator
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
 export default Navbar;
