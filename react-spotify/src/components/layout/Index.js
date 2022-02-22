9d15e087a61b3cc5648cbafc8833912088f0e57c
D.Hamza <dahhani.hamza1998@gmail.com>
2022-02-22 13:23:17 +0100

    add logic to handle switching between pages as tabs

--- react-spotify/src/components/layout/Index.js
+++ react-spotify/src/components/layout/Index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import SongList from '../music/SongList';
 import SongGenerator from '../generator/SongGenerator';
+import { Tabs, Tab } from './Tabs';
 
 const Index = () => {
   return (
     <React.Fragment>
+      <h1 className='text-center mb-4'>songs</h1>
+      <Tabs>
+        <Tab label='Tab 1'></Tab>
+        <Tab label='Tab 1'></Tab>
+        <Tab label='Tab 1'></Tab>
+      </Tabs>
       <SongGenerator />
       <SongList />
     </React.Fragment>
