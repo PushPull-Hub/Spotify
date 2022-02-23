706c169db1a7341175a54572a87b75e4595c5ba9
D.Hamza <dahhani.hamza1998@gmail.com>
2022-02-23 14:54:39 +0100

    move the navbar to a lower level component and change the way of passing data  to it

--- react-spotify/src/App.js
+++ react-spotify/src/App.js
@@ -1,15 +1,13 @@
-import Navbar from './components/layout/Navbar';
-import Index from './components/layout/Index';
 import React from 'react';
-import { Provider } from './store/Context';
 import { BrowserRouter as Router } from 'react-router-dom';
+import Index from './components/layout/Index';
+import { Provider } from './store/Context';
 
 function App() {
   return (
     <Provider>
       <Router>
         <React.Fragment>
-          <Navbar />
           <Index />
         </React.Fragment>
       </Router>
--- react-spotify/src/components/layout/Index.js
+++ react-spotify/src/components/layout/Index.js
@@ -1,26 +1,40 @@
 import React, { useContext } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import SongList from '../music/SongList';
-import favourites from '../favourites/favourites';
+import Favourites from '../favourites/favourites';
 import SongGenerator from '../generator/SongGenerator';
 import { Context } from '../../store/Context';
+import Navbar from '../layout/Navbar';
 
 const Index = () => {
   const state = useContext(Context);
-  return (
-    <React.Fragment>
-      <div className='container'>
-        <h1 className='text-center mb-4'>
-          {state ? state.selectedTab.title : 'Loading'}
-        </h1>
-        <Switch>
-          <Route exact path='/' component={SongList}></Route>
-          <Route exact path='/favs' component={favourites}></Route>
-          <Route exact path='/generator' component={SongGenerator}></Route>
-        </Switch>
-      </div>
-    </React.Fragment>
-  );
+
+  const setToActive = (tab) => {
+    state.changeActiveTab(tab);
+  };
+
+  if (state && state.tabs) {
+    const tabs = state.tabs;
+    return (
+      <React.Fragment>
+        <Navbar tabs={tabs} setToActive={setToActive} />
+        <div className={'container ' + state.selectedTab.theme}>
+          <h1 className='text-center mb-4'>{state.selectedTab.title}</h1>
+          <Switch>
+            <Route exact path='/' component={SongList}></Route>
+            <Route exact path='/favourites' component={Favourites}></Route>
+            <Route
+              exact
+              path='/songGenerator'
+              component={SongGenerator}
+            ></Route>
+          </Switch>
+        </div>
+      </React.Fragment>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Index;
