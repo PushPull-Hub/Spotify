1dce91dcb938543d67a740c9340a77c33f0c63ed
D.Hamza <dahhani.hamza1998@gmail.com>
2022-02-26 18:31:48 +0100

    chenge the rendering cases sort

--- react-spotify/src/components/Tab1/Song.js
+++ react-spotify/src/components/Tab1/Song.js
@@ -1,49 +1,44 @@
 import React from 'react';
 
 function Song({ song, changeSongStatue }) {
-  const handleHeartIconClick = (id) => {
-    changeSongStatue(id);
-  };
-
-  if (song) {
-    return (
-      <div className='col-md-6'>
-        <div className='card mb-4 shadow-sm'>
-          <div className='card-body'>
-            <div className='d-flex justify-content-between'>
-              <h5>{song.track_name}</h5>
-              <button
-                type='button'
-                className='btn '
-                onClick={() => handleHeartIconClick(song.id)}
-              >
-                <strong>
-                  <i
-                    className='fas fa-heart'
-                    style={{ color: song.isOneOfFav ? 'red' : 'black' }}
-                  />
-                </strong>
-              </button>
-            </div>
-
-            <p className='card-text'>
+  if (!song) {
+    return null;
+  }
+  return (
+    <div className='col-md-6'>
+      <div className='card mb-4 shadow-sm'>
+        <div className='card-body'>
+          <div className='d-flex justify-content-between'>
+            <h5>{song.track_name}</h5>
+            <button
+              type='button'
+              className='btn '
+              onClick={() => changeSongStatue(song.id)}
+            >
               <strong>
-                <i className='fas fa-play' /> Artist
+                <i
+                  className='fas fa-heart'
+                  style={{ color: song.isOneOfFav ? 'red' : 'black' }}
+                />
               </strong>
-              : {song.artist}
-              <br />
-              <strong>
-                <i className='fas fa-compact-disc' /> Published
-              </strong>
-              : {song.publishedThe}
-            </p>
+            </button>
           </div>
+
+          <p className='card-text'>
+            <strong>
+              <i className='fas fa-play' /> Artist
+            </strong>
+            : {song.artist}
+            <br />
+            <strong>
+              <i className='fas fa-compact-disc' /> Published
+            </strong>
+            : {song.publishedThe}
+          </p>
         </div>
       </div>
-    );
-  } else {
-    /* spinner */
-  }
+    </div>
+  );
 }
 
 export default Song;
