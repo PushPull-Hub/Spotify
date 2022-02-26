451ba830669bdb114d1c411c912de5bdef3d0ce2
D.Hamza <dahhani.hamza1998@gmail.com>
2022-02-26 18:29:00 +0100

    refactor the context component to a hook that contains arrow functions

--- react-spotify/src/store/Context.js
+++ react-spotify/src/store/Context.js
@@ -1,71 +1,38 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { SongApi, ConfigurationsApi } from '../components/helper/Apis';
 
 export const Context = React.createContext();
 
-export class Provider extends Component {
-  constructor(props) {
-    super(props);
-    this.updateState = this.updateState.bind(this);
-  }
+export function Provider({ children }) {
+  const [state, setState] = useState();
 
-  componentDidMount() {
-    this.initializeState()
-      .then((state) => this.setState(state))
-      .catch((error) => console.error(error));
-  }
+  useEffect(() => {
+    initializeState();
+  }, []);
 
-  async initializeState() {
-    return new Promise(async (resolve) => {
-      try {
-        const songsList = await this.getSongsPromise();
-        const configs = await this.getConfigurationsPromise();
-        const updateState = this.updateState;
-        resolve({ songsList, configs, updateState });
-      } catch (error) {
-        console.error(error);
-        resolve(null);
-      }
-    });
-  }
+  const initializeState = async () => {
+    const songsList = await getSongsList();
+    const configs = await getConfigurations();
+    setState({ songsList, configs, updateState });
+  };
 
-  getSongsPromise() {
-    return new Promise(async (resolve) => {
-      try {
-        const songApi = new SongApi();
-        const response = await songApi.getSongList();
-        response && response.data ? resolve(response.data) : resolve(null);
-      } catch (error) {
-        console.error(error);
-        resolve(null);
-      }
-    });
-  }
+  const getSongsList = async () => {
+    const songApi = new SongApi();
+    const response = await songApi.getSongList();
+    return response?.data;
+  };
 
-  getConfigurationsPromise() {
-    return new Promise(async (resolve) => {
-      try {
-        const configsApi = new ConfigurationsApi();
-        const response = await configsApi.getAppConfigurations();
-        response && response.data ? resolve(response.data) : resolve(null);
-      } catch (error) {
-        console.error(error);
-        resolve(null);
-      }
-    });
-  }
+  const getConfigurations = async () => {
+    const configsApi = new ConfigurationsApi();
+    const response = await configsApi.getAppConfigurations();
+    return response?.data;
+  };
 
-  updateState(newState) {
-    this.setState(newState);
-  }
+  const updateState = (newState) => {
+    setState((state) => ({ ...state, newState }));
+  };
 
-  render() {
-    return (
-      <Context.Provider value={this.state}>
-        {this.props.children}
-      </Context.Provider>
-    );
-  }
+  return <Context.Provider value={state}>{children}</Context.Provider>;
 }
 
 export const Consumer = Context.Consumer;
